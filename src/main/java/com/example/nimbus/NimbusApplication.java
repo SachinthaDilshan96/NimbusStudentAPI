a1c1e539a192e7ce535eccbd52bd74d28f65f636
sachinthadilshan <sachinthadilshan96@gmail.com>
2023-09-25 09:09:58 +0530

    swagger and liquibase added

--- src/main/java/com/example/nimbus/NimbusApplication.java
+++ src/main/java/com/example/nimbus/NimbusApplication.java
@@ -1,9 +1,24 @@
 package com.example.nimbus;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Student API",
+                version = "1.0.0",
+                description = "This api is for CRUD operations of students",
+                contact = @Contact(
+                        name = "Sachintha Dilshan",
+                        email = "sachinthadilshan96@gmail.com"
+                )
+        )
+)
 public class NimbusApplication {
 
     public static void main(String[] args) {
